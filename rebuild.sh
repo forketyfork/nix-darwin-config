0a69d811e177dba49275a06272fef7c93e47c33a
Forketyfork <forketyfork@icloud.com>
2024-06-12 09:43:05 +0200

    test

--- rebuild.sh
+++ rebuild.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 set -e
 pushd ~/dev/github/forketyfork/nix-darwin-config
 nvim flake.nix
-alejandra . 
+alejandra .
 git diff -U0 ./*.nix
 echo "darwin-rebuild..."
-darwin-rebuild switch --flake .#work &> nixos-switch.log || (grep --color error nixos-switch.log && false)
+darwin-rebuild switch --flake .#work &>nixos-switch.log || (grep --color error nixos-switch.log && false)
 gen=$(darwin-rebuild --list-generations | grep current)
 git commit -am "$gen"
 popd
