078d57ddfbe6f12d7017b3bed595673008eda4da
Forketyfork <forketyfork@icloud.com>
2025-04-14 16:28:42 +0200

    123   2025-04-14 16:28:36   (current)

--- flake.nix
+++ flake.nix
@@ -250,6 +250,17 @@
       };
 
       launchd.user.agents = {
+        onepassword_ssh_auth_sock = {
+          serviceConfig = {
+            ProgramArguments = [
+              "/bin/sh"
+              "-c"
+              "/bin/ln -sf $HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock \$SSH_AUTH_SOCK"
+            ];
+            Label = "com.1password.SSH_AUTH_SOCK";
+            RunAtLoad = true;
+          };
+        };
         ollama = {
           command = "${pkgs.ollama}/bin/ollama serve";
           serviceConfig = {
